4b5c3780eb165c908200991e9fa6c91754f97b91
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-17 20:00:21 -0500

    add spinner logic

---
+++ spinner1.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+  process.stdout.write("\r|     ");
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write("\r/     ");
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write("\r-     ");
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write("\r|    ");
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write("\r/    ");
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write("\r-    ");
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write("\r|    \n");
+}, 1700);
